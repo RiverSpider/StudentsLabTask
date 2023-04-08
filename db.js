cd12f3a0abd2ca415799533113762a1441fb4224
RiverSpider <raevskih8888@mail.ru>
2023-04-08 21:17:06 +0600

    Initial commit

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Client } = require('pg');
 const client = new Client({ 
-  user: 'postgres',
-  host: 'localhost',
-  database: 'feedback',
-  password: '',
+  user: 'riverspider',
+  host: 'dpg-cgoo9u8u9tun42q1o0ng-a',
+  database: 'feedback_gbp8',
+  password: 'DBCZMKBE1O6xRMwVcSIy2ylJWMlqa9Zh',
   port: 5432,
 });
 
